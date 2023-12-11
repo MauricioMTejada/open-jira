d588b6339112e53ee0eeed95500b6e2c58b44e9c
MauricioTejada <mauriciotejada14@gmail.com>
2023-12-11 17:34:58 -0300

    creación de archivos de 'Theme'

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext} from "next/document";
+
+class MyDocument extends Document {
+        static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
+
+    render() {
+        return (
+            <Html lang="es">
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
+                    />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
